cb6b0c557bb2c5583b55a8524ca90e6748ba7f7a
Steve Thomas <stevethomascl1221@gmail.com>
2026-02-04 20:45:58 +0530

    The Auth folder with complete process

--- lib/auth/auth_session_holder.dart
+++ lib/auth/auth_session_holder.dart
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-// import 'package:supabase_flutter/supabase_flutter.dart';
-
-// class AuthSessionHolder {
-//   static Session? session;
-// }
-
-=======
->>>>>>> 4201e22d3ff75ce6fd7d229a06adada811bebc6d
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthSessionHolder {
